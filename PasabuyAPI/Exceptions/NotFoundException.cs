aea99cbc049a87a24ad90dd7d3453d9244a5dc0e
jcatillo <jcatillo1121@gmail.com>
2025-10-13 00:56:45 +0800

    feat: added the following: NotFoundException and InvalidIdFormatException. Also integrated it in PaymentsRepository.

---
+++ PasabuyAPI/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+namespace PasabuyAPI.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base("Not Found") { }
+
+        public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string message, Exception inner) : base(message, inner) { }
+    }
+}
